871c3e5d835b5d4a8fb9549f2873067161deaa28
hanako-eo <enuxura@gmail.com>
2022-04-10 19:37:46 +0200

    feat: ajout d'un delta pour le callback

--- objects/Timer.py
+++ objects/Timer.py
@@ -7,17 +7,19 @@ class Timer:
   def update(self, delta: float):
     self.delta += delta
     if self.time <= self.delta:
+      self.callback(self.delta)
       self.delta = 0
-      self.callback()
 
 class Tick:
   def __init__(self, callback, tick):
     self.callback = callback
     self.tick = tick
     self.delta = 0
+    self.wait = 0
 
-  def update(self):
-    self.delta += 1
-    if self.tick <= self.delta:
-      self.delta = 0
-      self.callback()
+  def update(self, delta):
+    self.delta += delta
+    self.wait += 1
+    if self.tick <= self.wait:
+      self.callback(self.delta)
+      self.delta = self.wait = 0

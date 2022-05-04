0f833cf63abf5a0eb3d9ae11d3d14f910499c360
hanako-eo <enuxura@gmail.com>
2022-05-04 21:01:39 +0200

    fix: fight fonction des troupes valkyrie

--- entities/troops/Valkyrie.py
+++ entities/troops/Valkyrie.py
@@ -1,3 +1,4 @@
+from random import randint
 from constant import EMPTY
 from functions import w_range_y
 from entities.troops.Troop import Troop
@@ -11,13 +12,25 @@ class Valkyrie(Troop):
     self.attack = 30
     self.defence = 20
     self.watch_range = 2
+    self.y_search = list(map(lambda x: w_range_y(x)+y, range(0, self.watch_range+1)))
 
-  def search(self, x, y, direction):
+  def search(self, x, _):
     self.enemies = []
-    for i in range(0, self.watch_range+1):
-      future_cell = self.see(x + direction, y + w_range_y(i))
-      if future_cell != EMPTY and self.is_enemy != future_cell.is_enemy and not future_cell.state.match("dead"):
+    for y in self.y_search:
+      future_cell = self.see(x + self.direction, y)
+      if type(future_cell) != int and self.is_enemy != future_cell.is_enemy and not future_cell.state.match("dead"):
         self.enemies.append(future_cell)
+
+  def fight(self):
+    if len(self.enemies) <= 0:
+      self.action_queue(lambda: self.state.use("move", self))
+      
+    for enemy in self.enemies:
+      damage = self.attack - self.attack * (enemy.defence / 100)
+      enemy.life -= damage + min(damage / 4, 10) * (1 if randint(1, 100) <= 5 else 0)
+      self.life -= enemy.thorns * damage
+      if enemy.life <= 0 or enemy.state.match("dead") or enemy.x != self.x + self.direction:
+        self.enemies = list(filter(lambda _enemy: _enemy != enemy, self.enemies))
   
   def tiny_move():
     pass
